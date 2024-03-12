2326467d0a555c10104d46c8532f6b3b44f954bb
EvanLyu732 <zhengshushu1997@gmail.com>
2024-03-12 17:53:23 +0800

    feat: update config

--- early-init.el
+++ early-init.el
@@ -1,3 +1,8 @@
 ;; Elpaca required
 (setq package-enable-at-startup nil)
 
+;; proxy setting 
+(setq url-proxy-services
+      '(("http" . "127.0.0.1:7890")
+        ("https" . "127.0.0.1:7890")))
+
--- init.el
+++ init.el
@@ -89,3 +89,16 @@
 
 ;; ;; slime for common-lisp
 ;; (require 'slime)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ispell-dictionary nil)
+ '(package-selected-packages '(org-bullets ## org-modern slime cmake-mode compat)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
