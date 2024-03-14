55c862d874e3c2c37c0fd8792a01226a3b60d853
EvanLyu732 <zhengshushu1997@gmail.com>
2024-03-14 22:32:49 +0800

    update files 2024/03/014

--- early-init.el
+++ early-init.el
@@ -1,6 +1,9 @@
 ;; Elpaca required
 (setq package-enable-at-startup nil)
 
+;; all-the-icons-rendering
+(setq inhibit-compacting-font-caches t)
+
 ;; proxy setting 
 (setq url-proxy-services
       '(("http" . "127.0.0.1:7890")
